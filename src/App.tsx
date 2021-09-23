8affc66f528635dffb63be7c51d12a1e7159f104
Paul <paul.ned@outlook.com>
2021-09-23 13:14:55 +0200

    🚀 initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Products } from "./pages/Products";
+import { DashboardLayout } from "./pages/layout/DashboardLayout";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <DashboardLayout>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/products">
+            <Products />
+          </Route>
+        </Switch>
+      </DashboardLayout>
+    </>
   );
-}
-
-export default App;
+};
---
+++ src/pages/Products.tsx
@@ -0,0 +1,3 @@
+export const Products = () => {
+  return <p>Products</p>;
+};
