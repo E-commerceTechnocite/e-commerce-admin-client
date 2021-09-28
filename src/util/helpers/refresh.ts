b880e56ffe942d4810efabd1b773a6fbce8b38fe
JPEtienne <ji.pe.7@hotmail.com>
2021-09-28 17:11:04 +0200

    Working on Products page

---
+++ src/util/helpers/refresh.ts
@@ -0,0 +1,9 @@
+import { useHistory } from "react-router-dom"
+import { domain } from "../environnement"
+import { http } from "../http"
+
+const refresh = () => {
+  
+}
+
+export default refresh
--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -1,12 +1,68 @@
-import * as React from 'react';
+import * as React from "react"
+import { useEffect, useState } from "react"
+import { useHistory } from "react-router"
+import { domain } from "../util/environnement"
+import refresh from "../util/helpers/refresh"
+import { http } from "../util/http"
+import { IMeta } from "../util/interface/IMeta"
 
-interface IProductsProps {
+interface IProductsProps {}
+
+interface IProducts {
+  meta: IMeta
+  data: {
+    id: string
+    createdAt: string
+    deletedAt: string
+    title: string
+    reference: string
+    description: string
+    price: number
+    category: {
+      id: string
+      createdAt: string
+      updatedAt: string
+      deletedAt: string
+      label: string
+    }
+    taxRuleGroup: {
+      id: string
+    }
+  }
 }
 
 const Products: React.FunctionComponent<IProductsProps> = (props) => {
-  return (
-    <div className="products">This is the Product page</div>
-  );
-};
+  const [productsData, setProductsData] = useState<IProducts>()
 
-export default Products;
+  // console.log(productList)
+  const history = useHistory()
+
+  const onClick = () => {
+    console.log(productsData)
+  }
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("token")
+    const options = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+    http
+      .get<IProducts>(`${domain}/v1/product`, options)
+      .then(({ data, error }) => {
+        if (error) {
+        }
+        setProductsData(data)
+      })
+  }, [])
+
+  return (
+    <div className="products" onClick={onClick}>
+      {productsData && productsData.data[0].title}
+    </div>
+  )
+}
+
+export default Products
