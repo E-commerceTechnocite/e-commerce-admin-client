665d90eae7ace31ac29e8829d830409c9f183315
floleroy <florianleroy2004@gmail.com>
2021-09-29 10:19:02 +0200

    home page

---
+++ src/components/GraphicInformation.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const GraphicInformation: React.FunctionComponent = () => {
+  return  <div className="graphic-information">
+  <div className="wrap">
+      <div className="graphicItem graph1"></div>
+      <div className="graphicItem graph2"></div>     
+  </div>
+</div>
+};
+
+export default GraphicInformation;
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,4 +1,6 @@
 import * as React from "react"
+import DailyInformations from "../components/DailyInformations"
+import GraphicInformation from "../components/GraphicInformation"
 import Loading from "../components/Loading"
 import { http } from "../util/http"
 
@@ -7,7 +9,14 @@ interface IHomeProps {}
 const Home: React.FunctionComponent<IHomeProps> = (props) => {
   return (
     <>
-      <div className="home">This is the Home page</div>
+      <div className="home">
+        <DailyInformations/>
+        <GraphicInformation/>
+        <div className="productButtonContainer">
+            <h4>Last products added</h4>
+            <div className="action">PRODUCT+</div>
+        </div>
+      </div>
     </>
   )
 }
