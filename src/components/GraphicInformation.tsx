92ae622215995b69fafdd500f2ba033ffa059caa
floleroy <florianleroy2004@gmail.com>
2021-09-30 09:19:11 +0200

    par

--- src/components/GraphicInformation.tsx
+++ src/components/GraphicInformation.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 
 const GraphicInformation: React.FunctionComponent = () => {
-  return  <div className="graphic-information">
-  <div className="wrap">
-      <div className="graphicItem graph1"></div>
-      <div className="graphicItem graph2"></div>     
+  return (
+    <div className="graphic-information">
+      <div className="wrap">
+          <div className="graphicItem graph1"></div>
+          <div className="graphicItem graph2"></div>     
+      </div>
   </div>
-</div>
-};
-
+  )
+}
 export default GraphicInformation;
