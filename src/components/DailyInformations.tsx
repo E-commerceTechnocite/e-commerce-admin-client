a03c2e0c5f7e76ddfc186087154df02371c0a50f
JPEtienne <ji.pe.7@hotmail.com>
2021-10-14 14:17:16 +0200

    Rearranged home layout

--- src/components/DailyInformations.tsx
+++ src/components/DailyInformations.tsx
@@ -2,12 +2,11 @@ import * as React from 'react';
 
 const DailyInformations: React.FunctionComponent = () => {
   return (
-    <div className="daily-information"> 
-        <div className="wrap">
-            <div className="informationItem item1">
+    <div className="daily-information">
+            <div className="informationItem">
                 <div className="informationContainer">
                     <h4>Sales</h4>
-                    <p className="price">100 €</p>
+                    <p className="price">10321351 €</p>
                 </div>
                 <div>
                     <i className="fas fa-coins"></i>
@@ -15,8 +14,8 @@ const DailyInformations: React.FunctionComponent = () => {
             </div>
             <div className="informationItem">
                 <div className="informationContainer">
-                    <h4>Profit</h4>
-                    <p className="price">25 €</p>
+                    <h4>Transaction</h4>
+                    <p className="price">658</p>
                 </div>
                 <div>
                     <i className="fas fa-wallet"></i>
@@ -31,7 +30,7 @@ const DailyInformations: React.FunctionComponent = () => {
                     <i className="fas fa-users"></i>
                 </div>
             </div>
-            <div className="informationItem item4">
+            <div className="informationItem">
                 <div className="informationContainer">
                     <h4>New Users</h4>
                     <p className="price">45</p>
@@ -40,7 +39,6 @@ const DailyInformations: React.FunctionComponent = () => {
                     <i className="fas fa-user-plus"></i>
                 </div>
             </div>     
-        </div>
     </div>
   );
 };
