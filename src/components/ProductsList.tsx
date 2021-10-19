a81bd03310ea270cae0bbc92d2ab0c31cb3af65d
Paul <paul.ned@outlook.com>
2021-10-19 20:43:39 +0200

    Homepage and products animations

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,21 +1,22 @@
-import * as React from "react"
-import { useEffect, useState } from "react"
-import { useHistory } from "react-router"
-import { Link } from "react-router-dom"
-import Loading from "../components//loading/Loading"
-import Pagination from "../components/pagination/Pagination"
-import { PaginationMetadataModel } from "../models/pagination/pagination-metadata.model"
-import { PaginationModel } from "../models/pagination/pagination.model"
-import { ProductModel } from "../models/product/product.model"
-import { config } from "../index"
-import { sendRequest } from "../util/helpers/refresh"
-import { http } from "../util/http"
-import { htmlToText } from "html-to-text"
+import * as React from "react";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import { Link } from "react-router-dom";
+import Loading from "../components//loading/Loading";
+import Pagination from "../components/pagination/Pagination";
+import { PaginationMetadataModel } from "../models/pagination/pagination-metadata.model";
+import { PaginationModel } from "../models/pagination/pagination.model";
+import { ProductModel } from "../models/product/product.model";
+import { config } from "../index";
+import { sendRequest } from "../util/helpers/refresh";
+import { http } from "../util/http";
+import { htmlToText } from "html-to-text";
+import { motion } from "framer-motion";
 
 interface IProductsListProps {
-  number?: number
-  pagination?: boolean
-  success?: boolean | undefined
+  number?: number;
+  pagination?: boolean;
+  success?: boolean | undefined;
 }
 
 const ProductsList: React.FunctionComponent<IProductsListProps> = ({
@@ -23,64 +24,66 @@ const ProductsList: React.FunctionComponent<IProductsListProps> = ({
   pagination,
   success,
 }) => {
-  const [products, setProducts] = useState<ProductModel[]>()
-  const [meta, setMeta] = useState<PaginationMetadataModel>()
-  const [page, setPage] = useState<number>(1)
-  const [toast, setToast] = useState(false)
-  const [refreshPage, setRefreshPage] = useState(false)
-  const history = useHistory()
+  const [products, setProducts] = useState<ProductModel[]>();
+  const [meta, setMeta] = useState<PaginationMetadataModel>();
+  const [page, setPage] = useState<number>(1);
+  const [toast, setToast] = useState(false);
+  const [refreshPage, setRefreshPage] = useState(false);
+  const history = useHistory();
   // Request to get the page of the product list
   const pageRequest = () =>
     http.get<PaginationModel<ProductModel>>(
-      `${config.api}/v1/product?page=${page}${number ? "&limit=" + number : ""}`,
+      `${config.api}/v1/product?page=${page}${
+        number ? "&limit=" + number : ""
+      }`,
       {
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${sessionStorage.getItem("token")}`,
         },
       }
-    )
+    );
   const getProducts = async () => {
-    let { data, error } = await sendRequest(pageRequest)
+    let { data, error } = await sendRequest(pageRequest);
     if (error) {
-      history.push("/login")
+      history.push("/login");
     }
-    setProducts(data.data)
-    setMeta(data.meta)
-  }
+    setProducts(data.data);
+    setMeta(data.meta);
+  };
 
   const deleteRequest = (id: string) => {
     return http.delete(`${config.api}/v1/product/${id}`, null, {
       headers: {
         Authorization: `Bearer ${sessionStorage.getItem("token")}`,
       },
-    })
-  }
+    });
+  };
   const deleteProduct = async (id: string, title: string) => {
     if (confirm(`Delete product: ${title}?`)) {
-      let { error } = await sendRequest(deleteRequest, id)
+      let { error } = await sendRequest(deleteRequest, id);
       if (error) {
-        console.log(error.message)
-        history.push("/login")
+        console.log(error.message);
+        history.push("/login");
       }
-      setRefreshPage(!refreshPage)
+      setRefreshPage(!refreshPage);
     }
-  }
+  };
 
   // Check if product has been added and if so displays a toast
   useEffect(() => {
-    console.log(success)
+    console.log(success);
     if (success === true) {
-      setToast(true)
+      setToast(true);
       setTimeout(() => {
-        setToast(false)
-      }, 10000)
+        setToast(false);
+      }, 10000);
     }
-  }, [success])
+  }, [success]);
 
   useEffect(() => {
-    getProducts().then()
-  }, [page, refreshPage])
+    getProducts().then();
+  }, [page, refreshPage]);
 
   return (
     <>
@@ -88,7 +91,7 @@ const ProductsList: React.FunctionComponent<IProductsListProps> = ({
         <div className="top-container">
           {pagination && (
             <div className="search">
-              <i className="fas fa-search"></i>
+              <i className="fas fa-search" />
               <input type="text" placeholder="Search..." />
             </div>
           )}
@@ -114,54 +117,79 @@ const ProductsList: React.FunctionComponent<IProductsListProps> = ({
                 <span>Category</span>
                 <span>Price</span>
               </div>
-              {products.map((product) => {
-                var strippedHtml = htmlToText(product.description)
-                return (
-                  <div className="product" key={product.id}>
-                    {product.thumbnail && product.thumbnail.uri && (
-                      <span>
-                        <img
-                          src={config.api + product.thumbnail.uri}
-                          alt={product.thumbnail.title}
-                        />
-                      </span>
-                    )}
-                    {!product.thumbnail && (
-                      <span>
-                        <img />
-                      </span>
-                    )}
-                    <span>{product.title}</span>
-                    <span>{product.reference}</span>
-                    <span>
-                      {strippedHtml.length >= 100
-                        ? strippedHtml.substr(0, 50) + "..."
-                        : strippedHtml}
-                    </span>
-                    <span>{product.category.label}</span>
-                    <span>{product.price} €</span>
-                    <Link
-                      to={`/products/edit/${product.id}`}
-                      className="action"
+              <motion.div
+                variants={{
+                  hidden: { opacity: 0 },
+                  show: {
+                    opacity: 1,
+                    transition: {
+                      staggerChildren: 0.01,
+                    },
+                  },
+                }}
+                initial="hidden"
+                animate="show"
+              >
+                {products.map((product) => {
+                  const strippedHtml = htmlToText(product.description);
+                  return (
+                    <motion.div
+                      variants={{
+                        hidden: { opacity: 0 },
+                        show: { opacity: 1 },
+                      }}
+                      className="product"
+                      key={product.id}
                     >
-                      Edit
-                    </Link>
-                    <button
-                      className="delete"
-                      onClick={() => deleteProduct(product.id, product.title)}
-                    >
-                      <i className="fas fa-trash"></i>
-                    </button>
-                  </div>
-                )
-              })}
+                      {product.thumbnail && product.thumbnail.uri && (
+                        <span>
+                          <img
+                            src={config.api + product.thumbnail.uri}
+                            alt={product.thumbnail.title}
+                          />
+                        </span>
+                      )}
+                      {!product.thumbnail && (
+                        <span>
+                          <img />
+                        </span>
+                      )}
+                      <span>{product.title}</span>
+                      <span>{product.reference}</span>
+                      <span>
+                        {strippedHtml.length >= 100
+                          ? strippedHtml.substr(0, 50) + "..."
+                          : strippedHtml}
+                      </span>
+                      <span>{product.category.label}</span>
+                      <span>{product.price} €</span>
+
+                      <Link
+                        to={`/products/edit/${product.id}`}
+                        className="action"
+                      >
+                        Edit
+                      </Link>
+                      <motion.button
+                        whileHover={{
+                          scale: 1.1,
+                        }}
+                        className="delete"
+                        onClick={() => deleteProduct(product.id, product.title)}
+                      >
+                        <i className="fas fa-trash" />
+                      </motion.button>
+                    </motion.div>
+                  );
+                })}
+              </motion.div>
               {pagination && <Pagination meta={meta} pageSetter={setPage} />}
             </div>
           </>
         )}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ProductsList
+export default ProductsList;
