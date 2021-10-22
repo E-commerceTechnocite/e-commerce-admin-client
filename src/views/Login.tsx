6002008e107b9696d4474455cc9c81b69e998235
JPEtienne <ji.pe.7@hotmail.com>
2021-10-22 10:04:29 +0200

    Added prettier configuration file

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,155 +1,159 @@
-import * as React from "react";
-import { useEffect, useState } from "react";
-import { useHistory } from "react-router";
-import Loading from "../components/loading/Loading";
-import { http } from "../util/http";
-import { config } from "../index";
-import { auth, Permission } from "../util/helpers/auth";
-import { sendRequest } from "../util/helpers/refresh";
-import { Formik } from "formik";
-import { adminLoginSchema } from "../util/validation/loginValidation";
-import TextInput from "../components/inputs/TextInput";
-import PasswordInput from "../components/inputs/PasswordInput";
+import * as React from 'react'
+import { useEffect, useState } from 'react'
+import { useHistory } from 'react-router'
+import Loading from '../components/loading/Loading'
+import { http } from '../util/http'
+import { config } from '../index'
+import { auth, Permission } from '../util/helpers/auth'
+import { sendRequest } from '../util/helpers/refresh'
+import { Formik } from 'formik'
+import { adminLoginSchema } from '../util/validation/loginValidation'
+import TextInput from '../components/inputs/TextInput'
+import PasswordInput from '../components/inputs/PasswordInput'
 
 const Login: React.FunctionComponent = () => {
- const [checkbox] = useState(false);
- const [isPending, setIsPending] = useState(true);
- const [errorMessage, setErrorMessage] = useState<string>(null);
- const history = useHistory();
+  const [checkbox] = useState(false)
+  const [isPending, setIsPending] = useState(true)
+  const [errorMessage, setErrorMessage] = useState<string>(null)
+  const history = useHistory()
 
- /**
-  * Returns request for persmissions list
-  * @returns request
-  */
- const fetchPermissions = () =>
-  http.get<Permission[]>(`${config.api}/v1/o-auth/permissions`, {
-   headers: { ...auth.headers },
-  });
+  /**
+   * Returns request for persmissions list
+   * @returns request
+   */
+  const fetchPermissions = () =>
+    http.get<Permission[]>(`${config.api}/v1/o-auth/permissions`, {
+      headers: { ...auth.headers },
+    })
 
- /**
-  * Set permissions
-  * @param param0
-  * @returns
-  */
- const setPermissions = ({ data, error }) => {
-  if (!error) {
-   auth.permissions = data;
-   return history.push("/");
-  }
-  setIsPending(false);
- };
-
- // Check if the user is already logged
- useEffect(() => {
-  setIsPending(true);
-  if (auth.refresh) {
-   sendRequest(fetchPermissions)
-    .then(setPermissions)
-    .then(() => setIsPending(false));
-  } else {
-   fetchPermissions()
-    .then(setPermissions)
-    .then(() => setIsPending(false));
-  }
- }, []);
-
- /**
-  * Submits credentials for connection
-  * @param data
-  */
- const onSubmit = (data: { email: string; password: string }): void => {
-  setIsPending(true);
-  setErrorMessage(null);
-  const { email, password } = data;
-  const body = { email, password };
-  const options = {
-   headers: { "Content-Type": "application/json" },
-  };
-  http
-   .post<{ access_token: string; refresh_token: string }>(
-    `${config.api}/v1/o-auth/login`,
-    body,
-    options
-   )
-   .then(({ data, error }) => {
-    const { access_token, refresh_token } = data;
+  /**
+   * Set permissions
+   * @param param0
+   * @returns
+   */
+  const setPermissions = ({ data, error }) => {
     if (!error) {
-     auth.access = access_token;
-     auth.refresh = refresh_token;
-     sendRequest(fetchPermissions)
-      .then(({ data, error }) => {
-       auth.permissions = data;
-      })
-      .then(() => {
-       history.push("/");
-      });
-    } else {
-     if (error.statusCode === 400) {
-      setErrorMessage("Wrong email and/or password.");
-     }
-     setIsPending(false);
+      auth.permissions = data
+      return history.push('/')
     }
-   });
- };
+    setIsPending(false)
+  }
 
- return (
-  <>
-   {isPending && <Loading />}
-   {!isPending && (
-    <div className="login-admin">
-     <div>
-      <h2>SHOPTYK</h2>
-     </div>
-     <Formik
-      enableReinitialize
-      initialValues={{
-       email: "",
-       password: "",
-      }}
-      validationSchema={adminLoginSchema}
-      onSubmit={(data) => {
-       console.log(data);
-       onSubmit(data);
-      }}
-     >
-      {({ handleSubmit }) => {
-       return (
-        <>
-         <form onSubmit={handleSubmit}>
-          <p>
-           <span>Login</span> your account
-          </p>
-          <div className="email">
-           <TextInput name={"email"} label={"Email"} placeholder={"Email"} />
-           <i className="fas fa-envelope" />
-          </div>
-          <div className="password">
-           <PasswordInput
-            name={"password"}
-            label={"Password"}
-            placeholder={"Password"}
-           />
-           <i className="fas fa-lock" />
-          </div>
-          <div className="checkbox">
-           <input type="checkbox" id="checkbox" name="checkbox" />
-           <label htmlFor="checkbox">Remember me</label>
-          </div>
-          <input type="submit" value="Login" className="action" />
-          {errorMessage && (
-           <div className="login-error">
-            <div className="global-error">{errorMessage}</div>
-           </div>
-          )}
-         </form>
-        </>
-       );
-      }}
-     </Formik>
-    </div>
-   )}
-  </>
- );
-};
+  // Check if the user is already logged
+  useEffect(() => {
+    setIsPending(true)
+    if (auth.refresh) {
+      sendRequest(fetchPermissions)
+        .then(setPermissions)
+        .then(() => setIsPending(false))
+    } else {
+      fetchPermissions()
+        .then(setPermissions)
+        .then(() => setIsPending(false))
+    }
+  }, [])
 
-export default Login;
+  /**
+   * Submits credentials for connection
+   * @param data
+   */
+  const onSubmit = (data: { email: string; password: string }): void => {
+    setIsPending(true)
+    setErrorMessage(null)
+    const { email, password } = data
+    const body = { email, password }
+    const options = {
+      headers: { 'Content-Type': 'application/json' },
+    }
+    http
+      .post<{ access_token: string; refresh_token: string }>(
+        `${config.api}/v1/o-auth/login`,
+        body,
+        options
+      )
+      .then(({ data, error }) => {
+        const { access_token, refresh_token } = data
+        if (!error) {
+          auth.access = access_token
+          auth.refresh = refresh_token
+          sendRequest(fetchPermissions)
+            .then(({ data, error }) => {
+              auth.permissions = data
+            })
+            .then(() => {
+              history.push('/')
+            })
+        } else {
+          if (error.statusCode === 400) {
+            setErrorMessage('Wrong email and/or password.')
+          }
+          setIsPending(false)
+        }
+      })
+  }
+
+  return (
+    <>
+      {isPending && <Loading />}
+      {!isPending && (
+        <div className="login-admin">
+          <div>
+            <h2>SHOPTYK</h2>
+          </div>
+          <Formik
+            enableReinitialize
+            initialValues={{
+              email: '',
+              password: '',
+            }}
+            validationSchema={adminLoginSchema}
+            onSubmit={(data) => {
+              console.log(data)
+              onSubmit(data)
+            }}
+          >
+            {({ handleSubmit }) => {
+              return (
+                <>
+                  <form onSubmit={handleSubmit}>
+                    <p>
+                      <span>Login</span> your account
+                    </p>
+                    <div className="email">
+                      <TextInput
+                        name={'email'}
+                        label={'Email'}
+                        placeholder={'Email'}
+                      />
+                      <i className="fas fa-envelope" />
+                    </div>
+                    <div className="password">
+                      <PasswordInput
+                        name={'password'}
+                        label={'Password'}
+                        placeholder={'Password'}
+                      />
+                      <i className="fas fa-lock" />
+                    </div>
+                    <div className="checkbox">
+                      <input type="checkbox" id="checkbox" name="checkbox" />
+                      <label htmlFor="checkbox">Remember me</label>
+                    </div>
+                    <input type="submit" value="Login" className="action" />
+                    {errorMessage && (
+                      <div className="login-error">
+                        <div className="global-error">{errorMessage}</div>
+                      </div>
+                    )}
+                  </form>
+                </>
+              )
+            }}
+          </Formik>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Login
