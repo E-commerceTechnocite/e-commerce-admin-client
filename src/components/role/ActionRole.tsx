c861cda2c1d8c0bd813dc07d94719540a59efbbb
floleroy <florianleroy2004@gmail.com>
2021-10-26 17:11:09 +0200

    Finish Role

--- src/components/role/ActionRole.tsx
+++ src/components/role/ActionRole.tsx
@@ -3,67 +3,60 @@ import { useEffect, useState } from 'react'
 import { useHistory, useParams } from 'react-router'
 import { http } from '../../util/http'
 import { config } from '../../index'
-import { PaginationModel } from '../../models/pagination/pagination.model'
 import { sendRequest } from '../../util/helpers/refresh'
 import ArrowPrevious from '../previous/ArrowPrevious'
 import { RoleModel } from '../../models/role/role.model'
 
 interface IActionRoleProps {}
 
-/*interface InitialValues {
-  name: string
-  permissions: string
-}*/
-
 const ActionRole: React.FunctionComponent<IActionRoleProps> = () => {
   const history = useHistory()
   const [rolePermissions, setRolePermissions] = useState<string[]>([])
-  const [permissions, setPermissions] = useState([])
+  const [allPermissions, setAllPermissions] = useState([])
   const [myInputValue, setMyInputValue] = useState('')
   const [submitError, setSubmitError] = useState<string>(null)
-  //const [initialValues, setInitialValues] = useState<InitialValues>()
   const params: { slug: string } = useParams()
   const perms = {}
 
   const rolePostRequest = (data: RoleModel) => {
-        if (params.slug) {
-          console.log(params.slug) 
-          return http.patch(`${config.api}/v1/role/${params.slug}`, data, {
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${sessionStorage.getItem("token")}`,
-            },
-          })
-        }
-        return http.post(`${config.api}/v1/role`, data, {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
-          },
-        })
-    }
-
-    const submitRolePost = async (data: RoleModel) => {
-      setSubmitError(null)
-      console.log(data)
-      let { error } = await sendRequest(rolePostRequest, data)
-      if (error) {
-          history.push("/login")
-      }
-      history.push({
-          pathname: "/roles",
-          state: { success: true },
+    if (params.slug) {
+      console.log(params.slug) 
+      return http.patch(`${config.api}/v1/role/${params.slug}`, data, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+        },
       })
     }
+    return http.post(`${config.api}/v1/role`, data, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+      },
+    })
+  }
 
-    const onSubmit = (e: React.FormEvent): void => {
-        e.preventDefault()
-        const body = JSON.stringify({ name: myInputValue, permissions: rolePermissions }) as RoleModel
-        submitRolePost(body)
+  const submitRolePost = async (data: RoleModel) => {
+    setSubmitError(null)
+    console.log(data)
+    let { error } = await sendRequest(rolePostRequest, data)
+    if (error) {
+        history.push("/login")
     }
+    history.push({
+        pathname: "/roles",
+        state: { success: true },
+    })
+  }
+
+  const onSubmit = (e: React.FormEvent): void => {
+    e.preventDefault()
+    const body = JSON.stringify({ name: myInputValue, permissions: rolePermissions }) as RoleModel
+    submitRolePost(body)
+  }
 
   const permissionsRequest = () => {
-    return http.get<React.SetStateAction<any>>(`${config.api}/v1/role/permissions`, {
+    return http.get<string[]>(`${config.api}/v1/role/permissions`, {
       headers: {
         Authorization: `Bearer ${sessionStorage.getItem('token')}`,
       },
@@ -75,15 +68,15 @@ const ActionRole: React.FunctionComponent<IActionRoleProps> = () => {
     if (error) {
       history.push('/login')
     }
-    setPermissions(data) //Error? NO IT WORKS!
+    setAllPermissions(data) 
   }
 
   const currentPermissionsRequest = () => {
-    return http.get<React.SetStateAction<any>>(`${config.api}/v1/role/${params.slug}`, {
+    return http.get<RoleModel>(`${config.api}/v1/role/${params.slug}`, {
       headers: {
         Authorization: `Bearer ${sessionStorage.getItem("token")}`,
       },
-  })
+    })
   }
 
   const SubmitCurrentUser = async () => {
@@ -91,155 +84,145 @@ const ActionRole: React.FunctionComponent<IActionRoleProps> = () => {
     if (error) {
       history.push("/login")
     }
-    //setPermissions(data.permissions)
-    setRolePermissions((permission) => [...permission, data.permissions])
-    console.log(permissions)
-    /*const allCheckbox : any = document.querySelectorAll('input[name=toggleAll]')
-    allCheckbox[0].checked = true*/
-    const name : any = document.querySelectorAll('input[name=name]');
-    console.log(data.name)
-    name[0].value = data.name
+    setRolePermissions(data.permissions)
+    setName(data.name)
     initializeCheckboxes(data.permissions)
   }
-
-  const initializeCheckboxes = (perm) => {
-    console.log(permissions)
-    perm.map((perm) => {
-      const check : any = document.querySelectorAll("[id='" + perm +"']")
-      for (let i = 0; i < check.length; i++) {
-        check[i].checked = true
-      }
-    })
-  }
-
-  useEffect(() => {
-    SubmitPermissions().then()
-    if (params.slug) {
-      SubmitCurrentUser().then()
-    }
-  }, [params.slug])
 
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1)
   }
 
-  permissions.forEach((item) => {
-    let [operation, entity] = item.split(':')
-    const capitalizeEntity = capitalizeFirstLetter(entity)
-    const entityDash = capitalizeEntity
-    entity = entity.replace(/-/g, ' ') //replace dash between words with space
-    let name = null
+  const crudTogglePermsCheckbox = (permsCheckbox) => {
+    let counter = 0
+    for (let i = 0; i < permsCheckbox.length; i++) {
+      console.log(permsCheckbox[i].checked)
+      if (i !== 0 && permsCheckbox[i].checked)
+        counter++
+    }
+    if(counter === 4) 
+      permsCheckbox[0].checked = true
+    else
+      permsCheckbox[0].checked = false
+  }
 
-    if (!perms[entityDash]) perms[entityDash] = []
+  const setName = (name) => {
+    const nameLabel : any = document.querySelectorAll('input[name=name]')
+    nameLabel[0].value = name
+    setMyInputValue(name)
+  }
 
-    if (operation === 'r') name = 'Read'
-    if (operation === 'c') name = 'Create'
-    if (operation === 'u') name = 'Update'
-    if (operation === 'd') name = 'Delete'
+  const initializeCrudCheckboxes = (perm) => {
+    perm.map((permi) => {
+      const check : any = document.querySelectorAll("[id='" + permi +"']")
+      check[0].checked = true
+    })
+  }
 
-    perms[entityDash].push({
+  const initializePermsCheckboxes = (perms) => {
+    perms.map((perm) => {
+      let [operation, title] = perm.split(':')
+      const permsCheckbox : any = document.querySelectorAll('input[name=' + title + ']')
+      crudTogglePermsCheckbox(permsCheckbox)
+    })
+  }
+
+  const initializeCheckboxes = (perm) => {
+    initializeCrudCheckboxes(perm)
+    initializePermsCheckboxes(perm)
+  }
+  
+  useEffect(() => {
+    const allCheckbox : any = document.querySelectorAll('input[name=toggleAll]')
+    if(rolePermissions.length === allPermissions.length && rolePermissions.length !== 0) {
+      allCheckbox[0].checked = true // check the select all checkbox if all checkboxes are checked 
+    }
+    else
+      allCheckbox[0].checked = false
+    
+    rolePermissions.map((perm) => {
+      let [operation, title] = perm.split(':')
+      const permsCheckbox : any = document.querySelectorAll('input[name=' + title + ']')
+      crudTogglePermsCheckbox(permsCheckbox)
+    })
+  }, [rolePermissions])
+
+  useEffect(() => {
+    SubmitPermissions().then()
+  }, [])
+
+  useEffect(() => {
+    if (params.slug) {
+      SubmitCurrentUser().then()
+    }
+  }, [params.slug])
+
+  const changeTitleForm = (title) => {
+    title = capitalizeFirstLetter(title)
+    title = title.replace(/-/g, ' ')
+    return title
+  }
+
+  const crudName = (operation) => {
+    if (operation === 'r') return 'Read'
+    if (operation === 'c') return 'Create'
+    if (operation === 'u') return 'Update'
+    if (operation === 'd') return 'Delete'
+  }
+
+  allPermissions.forEach((item) => {
+    const [operation, title] = item.split(':')
+    const name = crudName(operation)
+    if (!perms[title]) perms[title] = []
+    perms[title].push({
       value: item,
-      title: capitalizeEntity,
+      title: title,
       name: name,
     })
   })
 
-  const checkboxChange = (e: boolean, value: string) => {
-    if (e) setRolePermissions([...rolePermissions, value])
+  const checkboxClick = (onClick: boolean, value: string) => {
+    if(onClick) setRolePermissions([...rolePermissions, value])
     else setRolePermissions(rolePermissions.filter((item) => item !== value))
+  }
 
-    let [operation, entity] = value.split(':')
-    entity = capitalizeFirstLetter(entity)
-    const headCheckbox : any = document.querySelectorAll('input[name=' + entity + ']')
-    const allCheckbox : any = document.querySelectorAll('input[name=toggleAll]')
-    if(e) {
-      let counter = 0
-      for (let i = 0; i < headCheckbox.length; i++) {
-          if (headCheckbox[i].checked)
-              counter++
-      }
-      if(counter === 4) headCheckbox[0].checked = true
-      if(rolePermissions.length +1 === permissions.length) allCheckbox[0].checked = true
-    }
-      
-    if(!e) {
-      headCheckbox[0].checked = false
-      allCheckbox[0].checked = false
+  const changeAllCheckboxes = (checkboxes, source) => { //change multiple checkboxes for ( selectAll and Permission checkboxes)
+    for (let i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i] != source)
+        checkboxes[i].checked = source.checked;
     }
   }
 
   const toggleAll = (source) => {
-    const checkboxes : any = document.querySelectorAll('input[type="checkbox"]');
-    for (let i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i] != source)
-            checkboxes[i].checked = source.checked;
-    }
+    const checkboxes : any = document.querySelectorAll('input[type="checkbox"]')
+    changeAllCheckboxes(checkboxes, source)
     setRolePermissions([])
-    if(source.checked) setRolePermissions(permissions)
+    if(source.checked) setRolePermissions(allPermissions)
   }
 
   const togglePermsAll = (source, title) => {
-    title = title.replace(/ /g, '-')
-    const checkboxes : any = document.querySelectorAll('input[name=' + title + ']');
-    const allCheckbox : any = document.querySelectorAll('input[name=toggleAll]')
-    for (let i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i] != source)
-            checkboxes[i].checked = source.checked;
-    }
+    const checkboxes : any = document.querySelectorAll('input[name=' + title + ']')
+    changeAllCheckboxes(checkboxes, source) // 4 crud check/uncheck if we check/uncheck permissions checkbox
+    
     if(source.checked) {
-      checkboxes.forEach((element) => {
+      checkboxes.forEach((element) => { // if the c,r,u,d is not checked yet we add the value in permissions array
         if (rolePermissions.indexOf(element.id) === -1) {
-          if(element.id !== "") setRolePermissions((permission) => [...permission, element.id])
-         }          
-      });
-      if(rolePermissions.length +4 === permissions.length) allCheckbox[0].checked = true
-    }
-    else {
-      allCheckbox[0].checked = false
-      checkboxes.forEach((element) => {
+          if(element.id !== "") 
+            setRolePermissions((permission) => [...permission, element.id])
+        }          
+      })
+    } else {
+      checkboxes.forEach((element) => { //if c,r,u,d is not unchecked we remove the value in the permissions array
         if (rolePermissions.indexOf(element.id) !== -1) {
           setRolePermissions((permissions) => permissions.filter((item) => item !== element.id))
-         }          
-      });
+        }          
+      })
     }
   }
 
-  /*const onSubmit = (e: React.FormEvent): void => {
-    e.preventDefault()
-    const body = JSON.stringify({
-      name: myInputValue,
-      permissions: rolePermissions,
-    })
-    const token = sessionStorage.getItem('token')
-    const options = {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-    }
-    if (params.slug) {
-      http.patch<{ access_token: string; refresh_token: string }>(
-        `${config.api}/v1/role/${params.slug}`,
-        body,
-        options
-      )
-    } else {
-      http.post<{ access_token: string; refresh_token: string }>(
-        `${config.api}/v1/role`,
-        body,
-        options
-      )
-    }
-    setSubmitError(null)
-    history.push({
-      pathname: '/roles',
-      state: { success: true },
-    })
-  }*/
-
   return (
     <>
-      {permissions && (
+      {allPermissions && (
         <>
           <ArrowPrevious />
           <form onSubmit={onSubmit}>
@@ -262,9 +245,10 @@ const ActionRole: React.FunctionComponent<IActionRoleProps> = () => {
               <div className="role-permissions">
                 <div className="role-permissions-title">
                   <h3>Role permissions</h3>
-                  <input className="toggleAll" type="checkbox" name="toggleAll" onChange={(e) => toggleAll(e.target)}/> 
-                    Toggle All
-                    <br/>
+                </div>
+                <div className="toggleAll">
+                  <input type="checkbox" name="toggleAll" onChange={(e) => toggleAll(e.target)}/> 
+                  Select All
                 </div>
                 <div className="permissions-list">
                   {Object.entries(perms).map(([title, arr], index) => {
@@ -273,7 +257,7 @@ const ActionRole: React.FunctionComponent<IActionRoleProps> = () => {
                         <div className="permissions-choices-title"> 
                           <h4>
                             <input type="checkbox" name={title} onChange={(e) => togglePermsAll(e.target,title)}/> 
-                              {title}
+                            {changeTitleForm(title)}
                           </h4>
                         </div>
                         <div className="attrs" id="attrs">
@@ -286,7 +270,7 @@ const ActionRole: React.FunctionComponent<IActionRoleProps> = () => {
                                     id={perm.value}
                                     name= {perm.title}
                                     onChange={(e) =>
-                                      checkboxChange(
+                                      checkboxClick(
                                         e.target.checked,
                                         perm.value
                                       )
