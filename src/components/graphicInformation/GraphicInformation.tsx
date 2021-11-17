413a7f911f986f39336c4e33a18bcf38d8332b77
JPEtienne <ji.pe.7@hotmail.com>
2021-11-17 09:20:10 +0100

    Small css changes

--- src/components/graphicInformation/GraphicInformation.tsx
+++ src/components/graphicInformation/GraphicInformation.tsx
@@ -113,13 +113,13 @@ const GraphicInformation: React.FunctionComponent = () => {
       <motion.div variants={items} className="graphicItem">
           <Line 
             data={salesState}
-            options={salesOptions}
+            // options={salesOptions}
           />
       </motion.div>
       <motion.div variants={items} className="graphicItem">
           <Bar
             data={usersState}
-            options={usersOptions}
+            // options={usersOptions}
           />
       </motion.div>
     </motion.div>
