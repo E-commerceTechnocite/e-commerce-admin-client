affe0d703739b2516be2c50b1cd307f9c2054a03
JPEtienne <ji.pe.7@hotmail.com>
2021-11-24 14:23:20 +0100

    :bug: oops

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import '../public/scss/index.scss'
+import './scss/index.scss'
 import { App } from './App'
 import { http } from './util/http'
 
